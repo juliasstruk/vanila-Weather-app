a1d59947ba4c673f6464f5f9b531d5ee3f79b614
Juli S <strukjulia@gmail.com>
2022-11-22 18:16:05 +0100

    Form Search

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ function formatDate(timestamp) {
         "Saturday"
     ];
     let day = days[date.getDay()];
-    return `${day} ${hours} ${minutes}`;
+    return `${day} ${hours}:${minutes}`;
 }
 
 
@@ -31,18 +31,39 @@ let descriptionElement = document.querySelector("#description");
 let humidityElement = document.querySelector("#humidity");
 let windElement = document.querySelector("#wind");
 let dateElement = document.querySelector("#date");
+let iconElement = document.querySelector("#icon");
+
+celsiusTemperature = response.data.main.temp;
+
 temperatureElement.innerHTML = Math.round (response.data.main.temp); 
 cityElement.innerHTML = response.data.name;
 descriptionElement.innerHTML = response.data.weather[0].description;
 humidityElement.innerHTML = response.data.main.humidity;
 windElement.innerHTML = Math.round (response.data.wind.speed);
 dateElement.innerHTML = formatDate (response.data.dt * 1000);
+iconElement.setAttribute (
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 
+function search(city) {
 let apiKey = "5e5c2757f7c28e9aed7d744b591dfdeb";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
-
-
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(displayTemperature);
+}
 
+function handleSubmit(event) {
+event.preventDefault();
+let cityInputElement = document.querySelector("#city-input");
+search(cityInputElement.value);
+}
+
+let form = document.querySelector ("#search-form");
+form.addEventListener ("submit", handleSubmit);
+
+search("New York");
