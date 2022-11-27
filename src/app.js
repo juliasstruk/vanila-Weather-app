ae52ac09a9c596c2c170132ac3d59dda6663579a
Juli S <strukjulia@gmail.com>
2022-11-27 21:20:34 +0100

    add daily forecast

--- src/app.js
+++ src/app.js
@@ -22,37 +22,38 @@ function formatDate(timestamp) {
     return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast () {
+function displayForecast(response) {
+    console.log(response.data.daily);
 let forecastElement = document.querySelector ("#forecast");
 
 let days = ["Thu", "Fri", "Sat", "Sun"];
 
 let forecastHTML = `<div class="row">`;
-days.forEach(function(day) {
+days.forEach(function (day) {
 forecastHTML = 
-forecastHTML + 
-`
-<div class="col-2">
+    `<div class="col-2">
     <div class="weather-forecast-date">${day}</div>
-    <img 
-    src="http://openweathermap.org/img/wn/01d@2x.png" alt="" width="42"/>
+    <img src="https://openweathermap.org/img/wn/01d@2x.png" alt="" width="42"/>
     <div class="weather-forecast-temperature">
  <span class="weather-forecast-temperatute-max"> 18° </span>
  <span class="weather-forecast-temperatute-min"> 12° </span>
 </div>
-</div>
-`;
+</div>`;
 });
 
 forecastHTML = forecastHTML + `</div>`;
 forecastElement.innerHTML = forecastHTML;
-console.log (forecastHTML);
 }
 
+function getForecast (coordinates) {
+    console.log(coordinates);
+    let apiKey = "5e5c2757f7c28e9aed7d744b591dfdeb";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
 
+    axios.get(apiUrl).then(displayForecast);
+}
 
 function displayTemperature(response){
-    console.log(response.data);
 let temperatureElement = document.querySelector("#temperature");
 let cityElement = document.querySelector("#city");
 let descriptionElement = document.querySelector("#description");
@@ -60,8 +61,6 @@ let humidityElement = document.querySelector("#humidity");
 let windElement = document.querySelector("#wind");
 let dateElement = document.querySelector("#date");
 let iconElement = document.querySelector("#icon");
-
-
 
 celsiusTemperature = response.data.main.temp;
 
@@ -110,9 +109,7 @@ celciusLink.classList.add ("active");
 fahrenheitlink.classList.remove ("active");
 let temperatureElement = document.querySelector ("#temperature");
 temperatureElement.innerHTML = Math.round(celsiusTemperature);
-
 }
-
 
 
 let celsiusTemperature = null;
@@ -127,7 +124,4 @@ fahrenheitlink.addEventListener("click", displayFahrenheitTemperature);
 let celciusLink = document.querySelector ("#celcius-link");
 celciusLink.addEventListener("click", displayCelsiusTemperature);
 
-
-
 search("New York");
-displayForecast ();
